53b5d00e1ce3d6d91247e9a1ecd0ebcdba8a2195
bakyt temishov <alekzandr14@gmail.com>
2023-11-08 19:22:35 +0100

    Создано приложение Counter

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var counterHistory: UITextView!
+    
+    let dateFormatter = DateFormatter()
+    var date: String = ""
+    var clickCounter: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        dateFormatter.dateStyle = .short
+        dateFormatter.timeStyle = .medium
+        dateFormatter.locale = Locale(identifier: "ru_Ru")
+        
     }
-
-
+    
+    @IBAction func countPlus(_ sender: Any) {
+        getDate()
+        clickCounter += 1
+        counterLabel.text = "Значение счётчика: \(clickCounter)"
+        counterHistory.text += "\n \(date) значение изменено на +1"
+    }
+    
+    @IBAction func countMinus(_ sender: Any) {
+        getDate()
+        if clickCounter > 0 {
+            clickCounter -= 1
+            counterLabel.text = "Значение счётчика: \(clickCounter)"
+            counterHistory.text += "\n \(date) значение изменено на -1"
+        } else {
+                counterHistory.text += "\n \(date) попытка уменьшить значение счётчика ниже 0"
+            }
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+        getDate()
+        clickCounter = 0
+        counterLabel.text = "Значение счётчика: \(clickCounter)"
+        counterHistory.text += "\n \(date) значение сброшено"
+    }
+    
+    func getDate() {
+        let todayDate = Date()
+        date = dateFormatter.string(from: todayDate)
+    }
+    
 }
 
